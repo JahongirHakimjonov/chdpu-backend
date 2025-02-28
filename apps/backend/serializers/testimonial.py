c4a3f34b1aeb0facf31c6636a7f021e18508ecf9
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-02-28 17:06:12 +0500

    Add ChairSerializer to TestimonialSerializer for nested representation

--- apps/backend/serializers/testimonial.py
+++ apps/backend/serializers/testimonial.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from apps.backend.models.testimonial import Testimonial
+from apps.backend.serializers.chair import ChairSerializer
 
 
 class TestimonialSerializer(serializers.ModelSerializer):
+    chair = ChairSerializer()
+
     class Meta:
         model = Testimonial
         fields = (
